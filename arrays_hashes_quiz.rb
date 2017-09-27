a614902eb6c5e311e6b7dcb62db5b9b47d11ce19
Huascar <huascarmc@hotmail.com>
2017-09-27 18:18:36 +0100

    deleted edinburgh park using index

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -30,7 +30,7 @@ puts lines.index("Edinburgh Park")
 puts lines.delete("Edinburgh Park")
 puts lines
 # 8. Delete 'Edinburgh Park' from the array by index
-
+lines.delete(2)
 # 9. Reverse the positions of the stops in the array
 
 ### B. Given the following data structure:
