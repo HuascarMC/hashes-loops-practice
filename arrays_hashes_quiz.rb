a012c80a8bb125cfd7654e0a59157e3c409b9572
Huascar <huascarmc@hotmail.com>
2017-09-27 18:20:26 +0100

    added returned strings from hash

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -39,11 +39,11 @@ puts lines
 my_hash = {"0" => "Zero", 1 => "One", :two => "Two", "two" => 2}
 
 # 1. How would you return the string `"One"`?
-
+puts my_hash["0"]
 # 2. How would you return the string `"Two"`?
-
+puts my_hash[1]
 # 3. How would you return the number `2`?
-
+puts my_hash[:two]
 # 4. How would you add `{3 => "Three"}` to the hash?
 
 # 5. How would you add `{:four => 4}` to the hash?
